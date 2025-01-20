e84dae1aaf6da1cd5102cfa0f08d2af373fee093
Veggieee <jungjaroen6879@gmail.com>
2025-01-20 16:42:30 +0700

    initial commit

---
+++ src/app/NewsList/page.jsx
@@ -0,0 +1,15 @@
+'use client'
+
+import React from 'react'
+import Link from 'next/link';
+
+const NewsList = () => {
+  return (
+    <div>
+      {/* <h1>NewsList</h1> */}
+      {/* <Link href='/' > DashBoard </Link> */}
+    </div>
+  );
+};
+
+export default NewsList;
