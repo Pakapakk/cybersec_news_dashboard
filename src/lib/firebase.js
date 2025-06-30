7106df0d4a926f85534b6d7d86e7c23af0cb1d9b
Veggieee <jungjaroen6879@gmail.com>
2025-06-30 22:06:22 +0700

    gauth fix

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -10,7 +10,7 @@ import { getAuth, GoogleAuthProvider, signInWithPopup, signInWithEmailAndPasswor
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   // authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  authDomain: "https://cybersec-news-dashboard.vercel.app",
+  authDomain: "cybersec-news-dashboard.vercel.app",
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
