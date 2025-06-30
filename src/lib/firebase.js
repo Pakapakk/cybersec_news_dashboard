a9b1395f24055fc4d56ca76409d5b484ca1f6386
Veggieee <jungjaroen6879@gmail.com>
2025-06-30 22:51:26 +0700

    fix auth

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -9,8 +9,8 @@ import { getAuth, GoogleAuthProvider, signInWithPopup, signInWithEmailAndPasswor
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  // authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  authDomain: "cybersec-news-dashboard.vercel.app",
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  // authDomain: "cybersec-news-dashboard.vercel.app",
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
